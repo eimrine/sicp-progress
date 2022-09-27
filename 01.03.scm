32e18347fbf96bf020b30776cd1cf5b3a533f5be
Eimrine's Point of View <eimrine@users.noreply.github.com>
2022-09-27 18:40:52 +0300

    Create 01.03.scm
    Упражнение 1.3.
    Определите процедуру, которая принимает в качестве аргументов три числа и возвращает сумму
    квадратов двух боґльших из них.

---
+++ 01.03.scm
@@ -0,0 +1,16 @@
+(define (procedure13 a b c)
+	(cond 	(
+				(and (<= a b)(<= a c))
+										(+ (* b b)(* c c)) 
+			)
+			(
+				(and (<= b a)(<= b c))
+										(+ (* a a)(* c c))	
+			)
+			(
+				(and (<= c a)(<= c b))
+										(+ (* a a)(* b b))
+			) 
+
+	)
+)
